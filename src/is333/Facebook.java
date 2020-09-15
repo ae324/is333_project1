66d36b6e858ef49759beee267737d24f7b8bd895
allis <ae324@njit.edu>
2020-09-15 08:29:12 -0400

    update

--- src/is333/Facebook.java
+++ src/is333/Facebook.java
@@ -21,7 +21,7 @@ public class Facebook {
     public static void main(String[] args) {
         System.out.println("connecting to Facebook");
         Facebook fb = new Facebook();
-        fb.createClient("EAA4cK0cc65YBAO2KT6x0ANgWtLDuNSWbnhheuNefum5k5ybrDizV9xhLfs3tY4w0JMbLWlFdKNUQm7x43vpZAZCZB78m9Rn2c5AutGCVaDPrGJy9rl6BwhDulnqgNZAU7opJdUgGmFiJTenG9E7Wmb4mikV85qT7G6RAbRce1hhjttPDO9ZCAat7GaZBDgmk33dRIUMG8Y8XBspPjMjJRkEP1L5A3FF8nHPMLZB3FYBZAQZDZD");
+        fb.createClient("EAA4cK0cc65YBAITZBC82OZC56ZBUqV6ZA6UHcltuWt72YkR7DUSmSJZCIK8CUyZA4gHHlFN5IQ0CdFguuNZCBv8VxHl47lYpbcqwk29vH339XZC1NzbcfvYzXMFk64XGM39JoTEYZBIuFU2xiZASoQhZCQNL3jP3zkSFTxKLQtcr030GGyhVi6BsVAfr3P6cy13o4gZD");
         fb.getMyInfo();
     }
 }
